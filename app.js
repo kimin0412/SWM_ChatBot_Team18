96dd43af015e4306ef7a064d072b226a55f89ae0
QuiD <wodnd101@naver.com>
2021-04-20 13:12:20 +0000

    URL/chatbot routing

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', index);
+app.use('/chatbot', index); // 최초 동작 라우팅 
+app.use('/', index); // request, callback 라우팅
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
