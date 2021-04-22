f521ca4ded6617087c9ed6a4674861d601d9152e
leehj8896 <leehj8896@gmail.com>
2021-04-22 15:10:59 +0000

    update romance stage 1

--- routes/themes/romance/stage1.js
+++ routes/themes/romance/stage1.js
@@ -1,12 +1,3 @@
-// const Config = require('config');
-// const axios = require('axios');
-// const kakaoInstance = axios.create({
-//   baseURL: 'https://api.kakaowork.com',
-//   headers: {
-//     Authorization: `Bearer ${Config.keys.kakaoWork.bot}`,
-//   },
-// });
-
 const libKakaoWork = require('/workspace/xkchyybcg/libs/kakaoWork');
 
 exports.sendMessage = async (message) => {
@@ -22,20 +13,3 @@ exports.sendMessage = async (message) => {
         ],
     });
 };
-// exports.openConversations = async ({ userId }) => {
-//   const data = {
-//     user_id: userId,
-//   };
-//   const res = await kakaoInstance.post('/v1/conversations.open', data);
-//   return res.data.conversation;
-// };
-
-// exports.sendMessage = async ({ conversationId, text, blocks }) => {
-//   const data = {
-//     conversation_id: conversationId,
-//     text,
-//     ...blocks && { blocks },
-//   };
-//   const res = await kakaoInstance.post('/v1/messages.send', data);
-//   return res.data.message;
-// };
