018a4309dff1e5f2e467bf3a7e1fa756aad4a51e
jb <calvin0627@naver.com>
2021-04-23 11:52:50 +0000

    add message, modal blocks template

---
+++ routes/themes/fantasy/blocks/intro.js
@@ -0,0 +1,28 @@
+module.exports = () => {
+	return {
+		text: 'Push alarm message',
+		blocks: [
+			{
+				type: 'header',
+				text: 'Fantasy Title',
+				style: 'blue',
+			},
+			{
+				type: 'image_link',
+				url:
+					'https://t1.kakaocdn.net/kakaowork/resources/block-kit/imagelink/image6@3x.jpg',
+			},
+			{
+				type: 'text',
+				text:
+					'배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명배경설명',
+				markdown: true,
+			},
+			{
+				type: 'button',
+				text: '게임 시작',
+				style: 'default',
+			},
+		],
+	};
+};
---
+++ routes/themes/fantasy/blocks/outtro.js
@@ -0,0 +1,27 @@
+module.exports = () => {
+	return {
+		text: 'Push alarm message',
+		blocks: [
+			{
+				type: 'text',
+				text: '축하합니다. 클리어하셨습니다.',
+				markdown: true,
+			},
+			{
+				type: 'action',
+				elements: [
+					{
+						type: 'button',
+						text: '랭킹 보기',
+						style: 'default',
+					},
+					{
+						type: 'button',
+						text: '테마선택',
+						style: 'default',
+					},
+				],
+			},
+		],
+	};
+};
