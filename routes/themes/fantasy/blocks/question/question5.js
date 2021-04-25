9a86c26d27aa3c3edcbd8d8a33b5d9e85496b8d1
jb <calvin0627@naver.com>
2021-04-25 09:34:29 +0000

    change some content

--- routes/themes/fantasy/blocks/question/question5.js
+++ routes/themes/fantasy/blocks/question/question5.js
@@ -16,7 +16,7 @@ module.exports = () => {
 			{
 				type: 'image_link',
 				url:
-					'https://i.ibb.co/2WgKdSW/q5.jpg', 
+					'https://i.ibb.co/84X8dZq/q5.jpg', 
 			},
 			{
 				type: 'action',
