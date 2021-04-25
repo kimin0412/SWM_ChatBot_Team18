2711ab20115801f30100b9c04d738bc9c0e0638c
jb <calvin0627@naver.com>
2021-04-25 09:32:11 +0000

    add image and button

--- routes/themes/fantasy/blocks/intro.js
+++ routes/themes/fantasy/blocks/intro.js
@@ -10,12 +10,12 @@ module.exports = () => {
 			{
 				type: 'image_link',
 				url:
-					'https://t1.kakaocdn.net/kakaowork/resources/block-kit/imagelink/image6@3x.jpg',
+					'https://i.ibb.co/Q9YKHKb/intro.png',
 			},
 			{
 				type: 'text',
 				text:
-					'부, 명성, 힘 모든 것을 손에 넣은 사나이 마에스트로, 그가 남긴 한마디는 전세계 사람들을 모험으로 나가게 만들었다.\n\n _내 보물말인가 원한다면 주지...\n 어디 찾아봐라,\n 이세상 모든 것을 그곳에 두고 왔다._ ',
+					'부, 명성, 힘 모든 것을 손에 넣은 사나이 마에스트로, 그가 남긴 한마디는 전세계 사람들을 모험으로 나가게 만들었다.\n\n _내 보물말인가 원한다면 주지...\n 어디 찾아봐라,\n 이세상 모든 것을 그곳에 두고 왔다.',
 				markdown: true,
 			},
 			{
