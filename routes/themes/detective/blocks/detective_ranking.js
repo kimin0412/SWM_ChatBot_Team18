5e064b08534a60ad26201a138ea502b0cbcb3926
LeeJiHoon <anteater1056@gmail.com>
2021-04-29 08:06:53 +0000

    탐정 테마 시작 화면 추가

--- routes/themes/detective/blocks/detective_ranking.js
+++ routes/themes/detective/blocks/detective_ranking.js
@@ -7,23 +7,30 @@ module.exports = async (data) => {
 	const user = await libDatabase.findUser(react_user_id);
 	
     const userName = user.userName;
-	const clearTime = user.themes.detective.dateCleared;
     
     const rankList = await libDatabase.getThemeRank('detective')
-    const rank = await libDatabase.getThemeUserRank(react_user_id, 'detective') + '등';
-
+    let rank = await libDatabase.getThemeUserRank(react_user_id, 'detective');
+	
+	if (rank) {
+		rank += '등'
+		clearTime = user.themes.detective.dateCleared;
+	} else {
+		rank = 'XX'
+		clearTime = 'TBD'
+	}
+	
     /** 랭킹 블록 */
     const rankListBlocks = [];
     let term, id, text;
     for (let i = 0; i < 5; i++) {
-        term = (i + 1) + '등'
+        term = (i + 1) + ' 등'
 
         if (i >= rankList.length) { // 랭킹에 사람이 적을 때 (5명 이하)
             text = 'TBD\nTBD'
         }
         else {
-            id = rankUser.userId.substr(rankUser.userId.length - 4);
-            text = `${rankUser.userName} (${id})\n${rankUser.themes.detective.dateCleared}`
+            id = rankList[i].userId.substr(rankList[i].userId.length - 4);
+            text = `${rankList[i].userName} (${id})\n${rankList[i].themes.detective.dateCleared}`
         }
 
         rankListBlocks.push({
@@ -57,15 +64,7 @@ module.exports = async (data) => {
         text: '방탈출 - 추리 테마',
         blocks: [
             { type: 'header', text: '명예의 전당 🏆', style: 'yellow' },
-            ...rankListBlocks,
-            {
-                type: 'button',
-                text: '테마 선택으로',
-                style: 'primary',
-                action_type: 'submit_action',
-                action_name: 'game_start',
-                value: 'game_start',
-            },
+            ...rankListBlocks
         ],
     };
 };
