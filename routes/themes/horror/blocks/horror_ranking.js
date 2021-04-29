dcf30900e9749781243532308847fb76dcf47451
김민지 <kimin0412@gmail.com>
2021-04-29 13:47:57 +0000

    Ranking Updated

--- routes/themes/horror/blocks/horror_ranking.js
+++ routes/themes/horror/blocks/horror_ranking.js
@@ -1,54 +1,127 @@
-const libDatabase = require('../../../../libs/database/').service
+const libDatabase = require('../../../../libs/database/').service;
 
 module.exports = async (data) => {
-    const { message, actions, action_time, value, action_name, react_user_id } = data;
-	
+	const { message, actions, action_time, value, action_name, react_user_id } = data;
+
 	const user = await libDatabase.findUser(react_user_id);
+
+	const userName = user.userName;
+
+	const rankList = await libDatabase.getThemeRank('horror');
+	let rank = await libDatabase.getThemeUserRank(react_user_id, 'horror');
+
+	if (rank) {
+		rank += '등';
+		clearTime = user.themes.horror.dateCleared;
+	} else {
+		rank = 'XX';
+		clearTime = 'TBD';
+	}
 	
-    const userName = user.userName;
-	const clearTime = user.themes.horror.dateCleared;
-    const rank = await libDatabase.getThemeUserRank(react_user_id, 'horror') + '등';
+	/** 랭킹 블록 */
+    const rankListBlocks = [];
+    let term, id, text;
+    for (let i = 0; i < 5; i++) {
+        term = (i + 1) + ' 등'
+
+        if (i >= rankList.length) { // 랭킹에 사람이 적을 때 (5명 이하)
+            text = 'TBD\nTBD'
+        }
+        else {
+            id = rankList[i].userId.substr(rankList[i].userId.length - 4);
+            text = `${rankList[i].userName} (${id})\n${rankList[i].themes.horror.dateCleared}`
+        }
+
+        rankListBlocks.push({
+            type: "description",
+            term: term,
+            content: {
+                type: "text",
+                text: text,
+                markdown: true
+            },
+            accent: true
+        });
+    }
+
+    // 내 등수 표시
+    // term = rank + '등'
+    id = user.userId.substr(user.userId.length - 4);
+    text = `${userName} (${id})\n${clearTime}`
+	
+    // rankListBlocks.push({
+    //     type: "description",
+    //     term: term,
+    //     content: {
+    //         type: "text",
+    //         text: text,
+    //         markdown: true
+    //     },
+    //     accent: true
+    // });
 	
     return {
-        text: '방탈출 - 추리 테마',
+        text: '방탈출 - 공포 테마',
         blocks: [
-            { type: 'header', text: '공포테마 클리어!', style: 'danger' },
-            {
-                type: 'description',
-                term: '이름',
-                content: { type: 'text', text: userName, markdown: false },
-                accent: true,
-            },
-            {
-                type: 'description',
-                term: '테마',
-                content: { type: 'text', text: '공포', markdown: false },
-                accent: true,
-            },
-            {
-                type: 'description',
-                term: '클리어 시각',
-                content: { type: 'text', text: clearTime, markdown: false },
-                accent: true,
-            },
-            {
-                type: 'description',
-                term: '현재 순위',
-                content: {
-                    type: 'text',
-                    text: rank,
-                    markdown: false,
-                },
-                accent: true,
-            },
-            {
-                type: 'button',
-                text: '테마 선택으로',
-                style: 'primary',
-                action_type: 'submit_action',
-                action_name: 'game_start',
-                value: 'game_start',
-            },
+            { type: 'header', text: '1등은 커피! 🏆', style: 'yellow' },
+            ...rankListBlocks,
+			{
+				type: 'divider',
+			},
+			{
+				type: "description",
+				term: "내 등수",
+				content: {
+					type: "text",
+					text: text,
+					markdown: true
+				},
+				accent: true
+			}
         ],
     };
 };
+
+// 	return {
+// 		text: '방탈출 - 추리 테마',
+// 		blocks: [
+// 			{ type: 'header', text: '공포 테마 클리어!', style: 'danger' },
+// 			{
+// 				type: 'description',
+// 				term: '이름',
+// 				content: { type: 'text', text: userName, markdown: false },
+// 				accent: true,
+// 			},
+// 			{
+// 				type: 'description',
+// 				term: '테마',
+// 				content: { type: 'text', text: '공포', markdown: false },
+// 				accent: true,
+// 			},
+// 			{
+// 				type: 'description',
+// 				term: '클리어 시각',
+// 				content: { type: 'text', text: clearTime, markdown: false },
+// 				accent: true,
+// 			},
+// 			{
+// 				type: 'description',
+// 				term: '현재 순위',
+// 				content: {
+// 					type: 'text',
+// 					text: rank,
+// 					markdown: false,
+// 				},
+// 				accent: true,
+// 			},
+// 			{
+// 				type: 'button',
+// 				text: '테마 선택으로',
+// 				style: 'primary',
+// 				action_type: 'submit_action',
+// 				action_name: 'game_start',
+// 				value: 'game_start',
+// 			},
+// 		],
+// 	};
+// };
