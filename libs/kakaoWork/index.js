cdba3fc0132285e5a5a1df536d8c3ef42d7d61c9
김민지 <kimin0412@gmail.com>
2021-04-30 05:35:48 +0000

    real final

--- libs/kakaoWork/index.js
+++ libs/kakaoWork/index.js
@@ -2,31 +2,36 @@ const Config = require('config');
 const axios = require('axios');
 
 const kakaoInstance = axios.create({
-  baseURL: 'https://api.kakaowork.com',
-  headers: {
-    Authorization: `Bearer ${Config.keys.kakaoWork.bot}`,
-  },
+    baseURL: 'https://api.kakaowork.com',
+    headers: {
+        Authorization: `Bearer ${Config.keys.kakaoWork.bot}`,
+    },
 });
 
 exports.getUserList = async () => {
-  const res = await kakaoInstance.get('/v1/users.list');
-  return res.data.users;
+    let res = await kakaoInstance.get('/v1/users.list?limit=100');
+    let users = res.data.users;
+    while (res.data.cursor) {
+        res = await kakaoInstance.get('/v1/users.list?cursor=' + res.data.cursor);
+        users = users.concat(res.data.users);
+    }
+    return users;
 };
 
 exports.openConversations = async ({ userId }) => {
-  const data = {
-    user_id: userId,
-  };
-  const res = await kakaoInstance.post('/v1/conversations.open', data);
-  return res.data.conversation;
+    const data = {
+        user_id: userId,
+    };
+    const res = await kakaoInstance.post('/v1/conversations.open', data);
+    return res.data.conversation;
 };
 
 exports.sendMessage = async ({ conversationId, text, blocks }) => {
-  const data = {
-    conversation_id: conversationId,
-    text,
-    ...blocks && { blocks },
-  };
-  const res = await kakaoInstance.post('/v1/messages.send', data);
-  return res.data.message;
+    const data = {
+        conversation_id: conversationId,
+        text,
+        ...(blocks && { blocks }),
+    };
+    const res = await kakaoInstance.post('/v1/messages.send', data);
+    return res.data.message;
 };
